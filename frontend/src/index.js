efb64945b11f78f90d150b74e80237f8d6b6e62f
Gilluan <gilluan@hotmail.com.br>
2018-03-18 20:07:45 -0300

    add graphql apollo client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,28 @@ import configureStore from './store/configureStore'
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
+//Add apollo
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+//TODO: substituir o link por um link de ambiente
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
 const store = configureStore()
 
 render(
-  <Router>
-    <Root store={store} />
-  </Router>,
+   <ApolloProvider client={client}>
+    <Router>
+      <Root store={store} />
+    </Router>
+    </ApolloProvider>,
   document.getElementById('root')
 )
 registerServiceWorker();
