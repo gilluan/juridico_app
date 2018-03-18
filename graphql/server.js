eb67fbd4a13a94f7365d53403ab70fb8fc3409ff
Gilluan <gilluan@hotmail.com.br>
2018-03-18 20:42:28 -0300

    add graphql integration

--- graphql/server.js
+++ graphql/server.js
@@ -3,10 +3,12 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
 import resolvers from './data/resolvers';
+import cors from 'cors';
 
-const GRAPHQL_PORT = 3000;
+const GRAPHQL_PORT = 4000;
 
 const graphQLServer = express();
+graphQLServer.use(cors());
 
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
